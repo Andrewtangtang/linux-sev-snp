f7f7e4479277ea747e3ffd3347665c81b3b93035
Al Viro <viro@zeniv.linux.org.uk>
2024-09-27 02:56:11 +0100

    finally take no_llseek out
    no_llseek had been defined to NULL two years ago, in commit 868941b14441
    ("fs: remove no_llseek")
    
    To quote that commit,
    
      At -rc1 we'll need do a mechanical removal of no_llseek -
    
      git grep -l -w no_llseek | grep -v porting.rst | while read i; do
    	sed -i '/\<no_llseek\>/d' $i
      done
    
      would do it.
    
    Unfortunately, that hadn't been done.  Linus, could you do that now, so
    that we could finally put that thing to rest? All instances are of the
    form
    	.llseek = no_llseek,
    so it's obviously safe.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/s390/crypto/pkey_api.c
+++ drivers/s390/crypto/pkey_api.c
@@ -2283,7 +2283,6 @@ static const struct attribute_group *pkey_attr_groups[] = {
 static const struct file_operations pkey_fops = {
 	.owner		= THIS_MODULE,
 	.open		= nonseekable_open,
-	.llseek		= no_llseek,
 	.unlocked_ioctl = pkey_unlocked_ioctl,
 };
 
--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -462,7 +462,6 @@ static void tty_show_fdinfo(struct seq_file *m, struct file *file)
 }
 
 static const struct file_operations tty_fops = {
-	.llseek		= no_llseek,
 	.read_iter	= tty_read,
 	.write_iter	= tty_write,
 	.splice_read	= copy_splice_read,
@@ -477,7 +476,6 @@ static const struct file_operations tty_fops = {
 };
 
 static const struct file_operations console_fops = {
-	.llseek		= no_llseek,
 	.read_iter	= tty_read,
 	.write_iter	= redirected_tty_write,
 	.splice_read	= copy_splice_read,
@@ -491,7 +489,6 @@ static const struct file_operations console_fops = {
 };
 
 static const struct file_operations hung_up_tty_fops = {
-	.llseek		= no_llseek,
 	.read_iter	= hung_up_tty_read,
 	.write_iter	= hung_up_tty_write,
 	.poll		= hung_up_tty_poll,
