0dde13dc988ab7cdb98ce9a43401dbd8996b83d8
headheadhead <b08902062@csie.ntu.edu.tw>
2025-05-05 09:48:40 +0800

    sockmap: queue psock->work into WQ_UNBOUND queue
    Based on the documentation of workqueue, a cpu intensive workloads
    should use WQ_UNBOUND queue so it could be better managed by the system
    scheduler.
    
    User could toggle the value of
    /sys/devices/virtual/workqueue/psock-wq/cpumask to bind the workqueue to
    a different core.

--- include/linux/skmsg.h
+++ include/linux/skmsg.h
@@ -141,6 +141,7 @@ int sk_msg_memcopy_from_iter(struct sock *sk, struct iov_iter *from,
 int sk_msg_recvmsg(struct sock *sk, struct sk_psock *psock, struct msghdr *msg,
 		   int len, int flags);
 bool sk_msg_is_readable(struct sock *sk);
+int psock_init(void);
 
 static inline void sk_msg_check_to_free(struct sk_msg *msg, u32 i, u32 bytes)
 {
