f269c7debc31e3e53e76e7ff46d6ca00ae32590d
headheadhead <b08902062@csie.ntu.edu.tw>
2026-02-21 11:30:41 +0800

    vhost-vsock: multi-queue support
    The patch introduces multi-queue support for vhost-vsock, which mostly
    refers to the design of vhost-net.
    
    The key difference is that each vhost-net virtqueue is backed by a
    TUN/TAP queue, which implicitly determines the vhost handler used for
    packet delivery. vhost-vsock has no equivalent multi-queue backend.
    Consequently, queue selection cannot rely on a backend device and must
    be handled explicitly within vhost-vsock.
    
    To address this, a new data structure (`struct vhost_vsock_pool`) is
    introduced to track all vhost-vsock handlers associated with a VM.
    This enables explicit handler (queue) selection in the host->guest data
    path.
    
    In the host->guest direction, the host must select an appropriate
    vhost-vsock handler when delivering a message to the guest. Unlike
    vhost-net -- where selecting a TAP queue implicitly selects the
    corresponding vhost handler -- vhost-vsock performs this selection
    directly using each VM's `vhost_vsock_pool`.
    
    Note that the limitation that one CID corresponds to one vhost-vsock is
    removed.
    
    The VMM (QEMU, crosvm, etc.) is expected to interact with vhost-vsock as
    follows:
    
    1. Open `/dev/vhost-vsock` multiple times to allocate resources (vhost
       thread, vhost virtqueues). For example, a vsock device with 8
       vq-pairs should open `/dev/vhost-vsock` 8 times.
    2. For each vhost-vsock handler return from `open("dev/vhost-vsock",
       ...)`, the VMM should configure the vring, the event notifier
       (guest->host), and the interrupt notifier (host->guest). Each handler
       only has two virtqueues (RX & TX).

--- net/vmw_vsock/virtio_transport_common.c
+++ net/vmw_vsock/virtio_transport_common.c
@@ -289,6 +289,8 @@ static struct sk_buff *virtio_transport_alloc_skb(struct virtio_vsock_pkt_info *
 
 	/* Set owner here, because '__zerocopy_sg_from_iter()' uses
 	 * owner of skb without check to update 'sk_wmem_alloc'.
+	 *
+	 * Notice that skb->hash is set during `skb_set_owner_w`.
 	 */
 	if (vsk)
 		skb_set_owner_w(skb, sk_vsock(vsk));
@@ -1334,6 +1336,7 @@ virtio_transport_recv_connecting(struct sock *sk,
 		sk->sk_socket->state = SS_CONNECTED;
 		vsock_insert_connected(vsk);
 		sk->sk_state_change(sk);
+		sk_set_txhash(sk);
 		break;
 	case VIRTIO_VSOCK_OP_INVALID:
 		break;
@@ -1585,6 +1588,7 @@ virtio_transport_recv_listen(struct sock *sk, struct sk_buff *skb,
 	release_sock(child);
 
 	sk->sk_data_ready(sk);
+	sk_set_txhash(sk);
 	return 0;
 }
 
