2d9f5548feabdc280d0cf46f595f4144be3fe5a7
headheadhead-csie <b08902062@csie.ntu.edu.tw>
2026-02-22 13:31:47 +0800

    Revert "skbuff: make sk_splice_from_iter() compound-awared"
    This commit introduces some bugs (unavailability of zero-copy redirect
    program for redis), simply revert it as a quick solution. Be aware that
    this commit was introduced in the first place to solve some performance
    issue. If similar problems occur, recall this patch.

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -7195,12 +7195,11 @@ ssize_t skb_splice_from_iter(struct sk_buff *skb, struct iov_iter *iter,
 			     ssize_t maxsize, gfp_t gfp)
 {
 	size_t frag_limit = READ_ONCE(net_hotdata.sysctl_max_skb_frags);
-	struct page *pages[MAX_SKB_FRAGS], **ppages = pages;
+	struct page *pages[8], **ppages = pages;
 	ssize_t spliced = 0, ret = 0;
 	unsigned int i;
 
 	while (iter->count > 0) {
-		struct page *last_head;
 		ssize_t space, nr, len;
 		size_t off;
 
@@ -7219,20 +7218,15 @@ ssize_t skb_splice_from_iter(struct sk_buff *skb, struct iov_iter *iter,
 		}
 
 		i = 0;
-		last_head = compound_head(pages[0]);
 		do {
 			struct page *page = pages[i++];
-			struct page *head = compound_head(page);
-			size_t part = min_t(size_t, PAGE_SIZE - (off & (PAGE_SIZE-1)), len);
+			size_t part = min_t(size_t, PAGE_SIZE - off, len);
 
 			ret = -EIO;
-			if (WARN_ON_ONCE(!sendpage_ok(head)))
+			if (WARN_ON_ONCE(!sendpage_ok(page)))
 				goto out;
 
-			if (head != last_head)
-				off = 0;
-
-			ret = skb_append_pagefrags(skb, head, off, part,
+			ret = skb_append_pagefrags(skb, page, off, part,
 						   frag_limit);
 			if (ret < 0) {
 				iov_iter_revert(iter, len);
@@ -7240,13 +7234,12 @@ ssize_t skb_splice_from_iter(struct sk_buff *skb, struct iov_iter *iter,
 			}
 
 			if (skb->ip_summed == CHECKSUM_NONE)
-				skb_splice_csum_page(skb, head, off, part);
+				skb_splice_csum_page(skb, page, off, part);
 
-			off += part;
+			off = 0;
 			spliced += part;
 			maxsize -= part;
 			len -= part;
-			last_head = head;
 		} while (len > 0);
 
 		if (maxsize <= 0)
